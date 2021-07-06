b1a6cd6bdd572316c321688445b8f07923367fae
Chris D'Aloisio <chris@creativecontrol.com.au>
2021-07-06 17:37:13 +1000

    chore: inline all calculations to the switch statement

--- spec/rental.spec.js
+++ spec/rental.spec.js
@@ -66,7 +66,27 @@ describe('Rental', function () {
     });
   });
 
-  describe('customer has a new movie', () => {
+  describe('customer has a new movie for 2 days or less', () => {
+    var properResult =
+      'Rental Record for martin' +
+      'Latest Hit Release     6' +
+      'Amount owed is 6' +
+      'You earned 1 frequent renter points';
+
+    it('should return the proper result', function () {
+      properResult = properResult.replace(/\s+/g, '');
+
+      var customer = {
+        name: 'martin',
+        rentals: [{ movieID: 'F004', days: 2 }]
+      };
+
+      var actualResult = stuff.statement(customer).replace(/\s+/g, '');
+      expect(actualResult).toEqual(properResult);
+    });
+  });
+
+  describe('customer has a new movie for more than 2 days', () => {
     var properResult =
       'Rental Record for martin' +
       'Latest Hit Release     9' +
--- src/rental.js
+++ src/rental.js
@@ -10,6 +10,7 @@ class Rental {
     let totalAmount = 0;
     let frequentRenterPoints = 0;
     let result = `Rental Record for ${customer.name}\n`;
+
     for (let r of customer.rentals) {
       const movie = movies[r.movieID];
       let thisAmount = 0;
@@ -21,29 +22,32 @@ class Rental {
           if (r.days > 2) {
             thisAmount += (r.days - 2) * 1.5;
           }
+          frequentRenterPoints += 1;
           break;
         case 'new':
           thisAmount = r.days * 3;
+          if (r.days > 2) {
+            frequentRenterPoints += 2;
+          } else {
+            frequentRenterPoints += 1;
+          }
           break;
         case 'childrens':
           thisAmount = 1.5;
           if (r.days > 3) {
             thisAmount += (r.days - 3) * 1.5;
           }
+          frequentRenterPoints += 1;
           break;
         default:
           throw new Error('Unknown movie type');
       }
 
-      // add frequent renter points
-      frequentRenterPoints += 1;
-      // add bonus for a two day new release rental
-      if (movie.code === 'new' && r.days > 2) frequentRenterPoints += 1;
-
       // print figures for this rental
       result += `\t${movie.title}\t${thisAmount}\n`;
       totalAmount += thisAmount;
     }
+
     // add footer lines
     result += `Amount owed is ${totalAmount}\n`;
     result += `You earned ${frequentRenterPoints} frequent renter points\n`;
